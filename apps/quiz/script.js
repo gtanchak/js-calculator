d6e0621f1bf7eff55034c5f80fd2d1370f2ae39d
gtanchak <ghtanchak77@gmail.com>
2025-11-19 21:35:19 +0530

    fix: working on the quiz app

--- apps/quiz/script.js
+++ apps/quiz/script.js
@@ -37,6 +37,8 @@ const isCorrect = (guess) => {
 // TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
 // TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
 
+for (let i = 0; i < 5; i++) {}
+
 // TODO 7: Within the event handler function,
 // Use a for loop to disable all the option buttons
 
